e8b16f944af5a7a777fb86b957221cca74085cfb
JAEJIN <wowls9416@naver.com>
2023-12-11 16:42:19 +0900

    에러핸들링 수정

--- src/middlewares/error-handling.middleware.js
+++ src/middlewares/error-handling.middleware.js
@@ -1,5 +1,8 @@
 export default function (err, req, res, next) {
   console.error(err);
 
-  res.status(500).json({ errorMessage: '서버 내부 에러가 발생했습니다.' });
+  const statusCode = err.statusCode ?? 500;
+  const message = err.message ?? '서버 내부 에러가 발생했습니다.';
+
+  res.status(statusCode).json({ message });
 }
